cb51fed7c524d650fc87a03363b05dd4fd5051d9
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-28 10:38:21 +0200

    [DOCS] primi dati stampati a schermo

--- app/Http/Controllers/TrainsController.php
+++ app/Http/Controllers/TrainsController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\train;
 use Illuminate\Http\Request;
 
 class TrainsController extends Controller
 {
-    //
+    public function index(){
+        //  dd('trains');
+        // $now = date('y-m-d',strtotime(now()));
+        $trains= train::all();
+        return view('home', compact('trains'));
+    }
 }
