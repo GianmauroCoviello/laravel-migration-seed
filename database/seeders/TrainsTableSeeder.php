1dde6e23b50e84d6982ae439168866d79536e9a4
GianmauroCoviello <gianmaurocoviello@gmail.com>
2023-07-30 22:54:01 +0200

    inseriti i faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+use App\Models\train;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +15,19 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<10; $i++) { 
+            $train = new train();
+            
+            $train->azienda = $faker->word();
+            $train->stazione_dipartenza->$faker->word();
+            $train->stazione_dipartenza->$faker->word();
+            $train->orario_di_partenza =  $faker->time();
+            $train->orario_di_arrivo =  $faker->time();
+            $train->numero_carrozze = $faker->numberBetween(1,19);
+            
+            $train->save();
+        }
     }
 }
